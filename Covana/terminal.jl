60aa4b6fc9c96e8eaca7b8586366c3d7acc7445c
MahanBi <dirtymbgamer@gmail.com>
2022-06-21 10:01:01 +0430

    Work On: CLI

--- Covana/terminal.jl
+++ Covana/terminal.jl
@@ -1,12 +1,12 @@
-using ArgParse
-using ProgressMeter
 using DocOpt
 
+const doc = """Usage:
+    train
+    converstation <portWA;LDNAND AWDNan DPNd
+    -h | --help | --version 
+"""
 
+using DocOpt
 
-
-
-function main()
-    
-end
-main()
+args = docopt(doc, version=v"0.1.1", exit_on_error=true, help=true)
+println(args)
